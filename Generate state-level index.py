4cca835f358a83322f37f3e94e3593de8d6d6794
Greg Schivley <gschivley@users.noreply.github.com>
2017-07-11 15:22:46 -0400

    Re-run with correct path

--- Generate state-level index.py
+++ Generate state-level index.py
@@ -9,22 +9,24 @@ import glob
 states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
 for state in ["DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]:
-    output_path = os.path.join('state gen data', state + ' fuels gen.csv')
+    output_path = os.path.join('Data storage',
+                               'state gen data', state + ' fuels gen.csv')
 
     # Need to change this if the large file is not in current folder
-    elec_path = '2017-05-25 ELEC.txt'
-    emission_factor_path = 'Final emission factors.csv'
+    elec_path = os.path.join('Data storage', '2017-05-25 ELEC.txt')
+    emission_factor_path = os.path.join('Data storage',
+                                        'Final emission factors.csv')
 
     # Creates state-level data from the bulk-download file
     state_total(state, elec_path, emission_factor_path, output_path)
 
     # Transforms the state-level gen
-    index_and_generation(facility_path='Facility gen fuels and CO2 '
-                         '2017-05-25.csv', all_fuel_path=output_path,
-                         epa_path='Monthly EPA emissions 2017-05-25.csv',
-                         emission_factor_path='Final emission factors.csv',
-                         export_folder='final state data', export_path_ext=' '
-                         + state, state=state)
+    # index_and_generation(facility_path='Facility gen fuels and CO2 '
+    #                      '2017-05-25.csv', all_fuel_path=output_path,
+    #                      epa_path='Monthly EPA emissions 2017-05-25.csv',
+    #                      emission_factor_path='Final emission factors.csv',
+    #                      export_folder='final state data', export_path_ext=' '
+    #                      + state, state=state)
 
 
 
