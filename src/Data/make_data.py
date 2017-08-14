46a90ab667f5552f58faf4f0150f7cb97ca6bef1
gschivley <gschivley@users.noreply.github.com>
2017-08-14 10:26:03 -0400

    Remove unnecessary imports

--- src/Data/make_data.py
+++ src/Data/make_data.py
@@ -3,11 +3,6 @@
 import os
 from os.path import join, abspath, normpath, dirname, split
 import pandas as pd
-# import zipfile
-# # from urllib import urlretrieve, urlopen
-# from urllib2 import urlopen
-# from cStringIO import StringIO
-# import requests
 
 def getParentDir(path, level=1):
     return normpath(join(path, *([".."] * level)))
