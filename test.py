4a8cda9f89c18207c994200a98964cb6da1286b0
gschivley <gschivley@users.noreply.github.com>
2017-08-14 13:40:42 -0400

    Changes to test file

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 from src.Data.make_data import get_annual_plants
 import os
+import pandas as pd
+from src.Analysis.state2nerc import annual
 
+annual_plants = get_annual_plants(2015)
+annual_plants.head()
 
-ap = get_annual_plants(2015)
-ap
+path = '/Users/Home/Documents/GitHub/Index-variability/Data storage/Facility gen fuels and CO2 2017-05-25.zip'
+df = pd.read_csv(path)
+df['state'] = df['geography'].str[-2]
+temp = df.loc[(df['plant id'].isin(annual_plants)) &
+                     (df['year'] == 2015)]
+annual(df=temp, state='TX')
+
+'reporting frequency' in temp.columns
