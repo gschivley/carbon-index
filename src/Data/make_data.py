7356da799c483a644684e6e4128d6559ee19a97e
gschivley <gschivley@users.noreply.github.com>
2017-08-14 10:14:26 -0400

    Improve function to get annual plants
    The function can now save a zip file to the correct data folder.
    
    Other bugs were squashed. It correctly rejects year inputs that are too
    recent (not final data), knows when a zip file already exists, and
    returns the correct data.

--- src/Data/make_data.py
+++ src/Data/make_data.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from os.path import join
+from os.path import join, abspath, normpath, dirname, split
 import pandas as pd
 # import zipfile
 # # from urllib import urlretrieve, urlopen
@@ -13,8 +13,7 @@ def getParentDir(path, level=1):
     return normpath(join(path, *([".."] * level)))
 
 def get_annual_plants(year,
-                      website='https://www.eia.gov/electricity/data/eia923/',
-                      base_path='~/Documents/Github/index-variability'):
+                      website='https://www.eia.gov/electricity/data/eia923/'):
     """
     Download the EIA-923 file for a given year if it doesn't already exist,
     and extract a list of plant ids for facilities that report annually. This
@@ -22,7 +21,9 @@ def get_annual_plants(year,
     links.
 
     inputs:
-        year: year of data to download
+        year: (int) year of data to download
+        website: (str) the website for EIA-923 data, which is used when
+            downloading zip files
 
     ouputs:
         annual_id: a Pandas Series with plant ids
@@ -31,18 +32,20 @@ def get_annual_plants(year,
     import glob
     import zipfile
 
-    # Testing abspath and relpath
-    return os.path.abspath(__file__), os.path.relpath(__file__)
+    unzip_path = None
+
+    # Get the project top-level path
+    ap = abspath(__file__)
+    top_path = getParentDir(dirname(ap), level=2)
 
     # Make folder if it doesn't exist
-    ## How to make this relative to wheever I'm running the function from?
-    path = join(os.abspath(base_path), 'Data storage', 'EIA downloads')
+    path = join(top_path, 'Data storage', 'EIA downloads')
     if not os.path.exists(path):
         os.mkdir(path)
 
     # Download the 923 zip file if it doesn't exist
     fname = 'f923_{}.zip'.format(year)
-    existing_zips = glob.glob(path)
+    existing_zips = [split(x)[-1] for x in glob.glob(join(path, '*.zip'))]
 
     if fname not in existing_zips:
         url = website + 'xls/{}'.format(fname)
@@ -52,19 +55,24 @@ def get_annual_plants(year,
         # Extract the zipfile into new folder if a filename contains "Final"
         z_file = zipfile.ZipFile(save_path)
         z_names = z_file.namelist()
-        if 'Final' in [segment for x in names for segment in x.split('_')]:
-            unzip_path = join(path, fname.split[0])
+        if 'Final' in [segment for x in z_names for segment in x.split('_')]:
+            unzip_path = join(path, fname.split('.')[0])
             z_file.extractall(unzip_path)
         else:
             raise ValueError('Final data is not available for {}'.format(year))
 
     # Read the sheet "Page 6 Plant Frame" from the appropriate file.
-    year_fn = glob.glob(join(unzip_path, '*Schedules_2*'))
-
+    if not unzip_path:
+        unzip_path = join(path, fname.split('.')[0])
+    year_fn = glob.glob(join(unzip_path, '*Schedules_2*'))[0]
     df = pd.read_excel(year_fn, sheetname='Page 6 Plant Frame', header=4)
-    df.columns = [col.lower() for col in df.columns]
 
+    # Column names in EIA documents can have line breaks and extra spaces
+    df.columns = [col.lower().replace('\n', ' ').strip() for col in df.columns]
+
+    # Get the plant ids for just plants that report annually
     annual_plants = df.loc[df['reporting frequency'] == 'A', 'plant id']
-    annual_plants.drop_duplicates(inplace=True)
+    annual_plants = (annual_plants.drop_duplicates()
+                                  .reset_index())
 
     return annual_plants
