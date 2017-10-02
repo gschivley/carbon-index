3790508b21e4c6a7624202effb0842277774749d
gschivley <gschivley@users.noreply.github.com>
2017-10-02 11:22:09 -0400

    Utility function to rename dataframe columns

--- src/util/utils.py
+++ src/util/utils.py
@@ -1,5 +1,23 @@
 import os
 from os.path import join, normpath
+import pandas as pd
 
 def getParentDir(path, level=1):
     return normpath(join(path, *([".."] * level)))
+
+def rename_cols(df, custom=None):
+    'If custom, use the custom dictionary'
+    if custom:
+        df.rename(columns=custom)
+    else:
+        # Make all columns lowercase
+        df.columns = df.columns.str.lower()
+
+        # Replace ORISPL_CODE with plant id
+        replace_dict = {'ORISPL_CODE': 'plant id'}
+
+        df.rename(columns=replace_dict)
+    pass
+
+def test_function():
+    print('this is a test function')
