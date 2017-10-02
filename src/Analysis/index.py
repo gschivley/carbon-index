df7fa9ef7d970420c81cee57fbb730f2f76595ad
gschivley <gschivley@users.noreply.github.com>
2017-10-02 11:24:34 -0400

    Start functions to calculate CO2 and gen/consumption by fuel

---
+++ src/Analysis/index.py
@@ -0,0 +1,233 @@
+# coding: utf-8
+
+# from __future__ import division
+import pandas as pd
+import os
+from os.path import join, abspath, normpath, dirname, split
+import numpy as np
+# from src.util.utils import rename_cols
+from src.util.utils import getParentDir, test_function, rename_cols
+import json
+
+# os.getcwd()
+#
+# path = 'Data storage/Facility gen fuels and co2 2017-05-25.zip'
+# df = pd.read_csv(path)
+# df.head()
+#
+# fuel_cat_path=join('Data storage',
+#                    'Fuel_categories.json')
+# top_path = getParentDir(dirname(ap), level=2)
+# path = '/Users/Home/Documents/GitHub/Index-variability/Data storage/Fuel categories/State_facility.json'
+# with open(path) as json_file:
+#     facility_fuel_cats = json.load(json_file)
+# type(facility_fuel_cats)
+# for a, b in facility_fuel_cats.items():
+#     print(a)
+#     print(b)
+
+def add_datetime(df, year='year', month='month'):
+    df['datetime'] = pd.to_datetime(df[year].astype(str) + '-' +
+                                    df[month].astype(str),
+                                    format='%Y-%m')
+
+def add_quarter(df, year='year', month='month'):
+    add_datetime(df, year, month)
+    df['quarter'] = df['datetime'].dt.quarter
+
+
+def facility_emission_gen(eia_facility, epa, ef, state_fuel_cat,
+                          custom_fuel_cat, export_state_cats=False):
+    """
+    Use EIA and EPA data to compile emissions, generation and fuel consumption
+    reported by facilities into emissions intensity and generation by fuel
+    category. Only facilities from the region of interest should be passed to
+    this function.
+
+    inputs:
+        eia_facility: (dataframe) monthly generation and fuel consumption as
+        reported by facilities to EIA
+        epa: (dataframe) monthly co2 emissions and gross generation as reported
+            by facilities to EPA
+        ef: (dataframe) emission factors for fuel combustion
+        state_fuel_cat (dict): match of state-level fuel categories to facility
+            level categories
+        custom_fuel_cat (dict): match of custom fuel categories for final
+            results to the state-level categories
+        export_state_cats (boolean): If co2 and gen should be exported at the
+            state category level
+
+    output:
+        dataframe: total adjusted co2 emissions, plus generation and fuel
+            consumption by fuel category
+    """
+    # Make column names consistent
+    rename_cols(eia_facility)
+    rename_cols(epa)
+
+    eia_grouped = group_facility_data(eia_facility)
+
+    epa_adj = adjust_epa_emissions(epa, eia_grouped)
+
+    # I need to return co2 (by facility, or just per month?) and gen/fuels by
+    # fuel type.
+
+    co2 = facility_co2(epa_adj, eia_grouped)
+    co2 = co2.loc[:, ['plant id', 'final co2 (kg)']]
+
+    gen_fuels_state = group_fuel_cats(eia_facility, state_fuel_cat)
+    if export_state_cats:
+        return co2, gen_fuels_state
+    else:
+        gen_fuels_custom = group_fuel_cats(gen_fuels_state, custom_fuel_cat)
+        return co2, gen_fuels_custom
+
+
+def group_facility_data(eia):
+    """
+    Group facility co2 emissions and generation data by plant id and calculate co2 ratio (elec/total)
+
+    inputs:
+        eia (df): data from EIA bulk download, including calculated co2
+            emissions (all total/fossil, elec total/fossil)
+
+    outputs:
+        grouped_df (df): grouped df with co2 emissions, generation, and a ratio
+            of co2 from electric fossil fuels to all total (fossil+bio) fuels
+    """
+    cols = ['all fuel fossil co2 (kg)', 'elec fuel fossil co2 (kg)',
+            'all fuel total co2 (kg)', 'elec fuel total co2 (kg)',
+            'generation (mwh)']
+    grouped_df = eia.groupby(['year', 'month', 'plant id'])[cols].sum()
+    grouped_df.reset_index(inplace=True)
+    grouped_df['co2 ratio'] = (grouped_df['elec fuel fossil co2 (kg)']
+                               / grouped_df['all fuel total co2 (kg)'])
+    grouped_df['co2 ratio'].fillna(0, inplace=True)
+
+    return grouped_df
+
+def adjust_epa_emissions(epa, eia_grouped):
+    """
+    Merge 2 dataframes and calculate an adjusted co2 emission for each facility.
+    This adjusted value accounts for CHP and biomass emissions using calculated
+    co2 emissions from fuel consumption.
+
+    inputs:
+        epa (df): monthly co2 emissions from each facility
+        eia_facility (df): grouped EIA facility data
+
+    outputs:
+        epa_adj (df):
+    """
+    eia_keep = ['month', 'year', 'all fuel total co2 (kg)',
+                'co2 ratio', 'plant id']
+
+    epa_adj = epa.merge(eia_facility[eia_keep],
+                        on=['plant id', 'year', 'month'], how='inner')
+
+    epa_adj.drop(['month', 'year', 'plant id'], axis=1, inplace=True)
+    epa_adj['epa index'] = (epa_adj.loc[:, 'co2_mass (kg)'] /
+                            epa_adj.loc[:, 'gload (MW)'])
+
+    # Start the adjusted co2 column with unadjusted value
+    epa_adj['adj co2 (kg)'] = epa_adj.loc[:, 'co2_mass (kg)']
+
+    # If CEMS reported co2 emissions are 0 but heat inputs are >0 and
+    # calculated co2 emissions are >0, change the adjusted co2 to NaN. These
+    # NaN values will be replaced by the calculated value later. Do the same
+    # for low index records (<300 g/kWh). If there is a valid co2 ratio,
+    # multiply the adjusted co2 column by the co2 ratio.
+
+    epa_adj.loc[~(epa_adj['co2_mass (kg)'] > 0) &
+                (epa_adj['heat_input (mmbtu)'] > 0) &
+                (epa_adj['all fuel total co2 (kg)'] > 0),
+                'adj co2 (kg)'] = np.nan
+    epa_adj.loc[(epa_adj['epa index'] < 300) &
+                (epa_adj['heat_input (mmbtu)'] > 0) &
+                (epa_adj['all fuel total co2 (kg)'] > 0),
+                'adj co2 (kg)'] = np.nan
+
+    epa_adj.loc[epa_adj['co2 ratio'].notnull(),
+                'adj co2 (kg)'] *= (epa_adj.loc[epa_adj['co2 ratio'].notnull(),
+                                                'co2 ratio'])
+
+    return epa_adj
+
+def facility_co2(epa_adj, eia_facility):
+    """
+    Merge the plant-level adjusted epa co2 emissions with generation. Create a
+    new column of final co2 emissions for each plant. Use calculated values from
+    eia fuel use where epa data don't exist.
+
+    inputs:
+        epa_adj (df): Reported EPA co2 emissions for each facility by month,
+            with a column for adjusted emissions
+        eia_facility (df): Fuel consumption, emissions, and generation by
+            facility
+
+    outputs:
+        df: merged dataframe with a "final co2 (kg)" column
+    """
+    merge_on = ['plant id', 'year', 'month']
+    df = eia_facility.merge(epa_adj, on=merge_on, how='left')
+
+    # keep the adjusted co2 column, but make a copy for final co2
+    df['final co2 (kg)'] = df.loc[:, 'adj co2 (kg)']
+
+    # Use calculated elec fossil co2 where adjusted epa values don't exist
+    df.loc[df['final co2 (kg)'].isnull(),
+           'final co2 (kg)'] = df.loc[df['final co2 (kg)'].isnull(),
+                                      'elec fuel fossil co2 (kg)']
+
+    return df
+
+def group_fuel_cats(df, fuel_cats):
+    """
+    Group fuels according to the fuel_cats dictionary inplace
+    """
+    for key, values in fuel_cats.items():
+        df.loc[df['fuel'].isin(values), 'type'] = key
+
+    df_grouped = df.groupby(['type', 'year', 'month']).sum()
+
+    return df_grouped
+
+def add_state_data(co2_gen, eia_total, ef):
+    """
+    Augment facility data with EIA estimates of non-reporting facilities. This
+    information is only available at the state level.
+
+    inputs:
+        co2_gen: (dataframe) adjusted co2 emissions, generation, and fuel
+            consumption from facilities.
+        eia_total: (dataframe) total generation and fuel consumption from all
+            facilities (including non-reporting), by state
+        ef: (dataframe) emission factors for fuel consumption
+
+    output:
+        dataframe: co2 emission intensity and generation by fuel category
+    """
+
+
+    return index_gen
+
+
+def co2_calc(fuel, ef):
+    """
+    Calculate co2 emissions based on fuel consumption using emission factors
+    from EIA/EPA and IPCC
+
+    inputs:
+        fuel: (dataframe) should have columns with the fuel type, total
+            consumption, and consumption for electricity generation
+        ef: (dataframe) emission factors (kg co2/mmbtu) for each fuel
+
+    output:
+        dataframe: co2 emissions from fuel consumption (total and for
+        electricity) at each facility
+    """
+
+
+    return co2
+
+# def add_nerc_data()
