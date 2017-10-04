ccfe092e035f8e964a293dd3a28c1b63b858a9be
gschivley <gschivley@users.noreply.github.com>
2017-10-04 11:44:59 -0400

    Add a function to add facility location to a dataframe (just a merge)

--- src/util/utils.py
+++ src/util/utils.py
@@ -17,8 +17,22 @@ def rename_cols(df, custom=None):
         # Make all columns lowercase
         df.columns = df.columns.str.lower()
 
-
     pass
 
-def test_function():
-    print('this is a test function')
+def add_facility_location(df, label_df, labels=[]):
+    """
+    Add location info (lat/lon, state, nerc, or other region) to a dataframe
+    with plant ids.
+
+    inputs:
+        df (df): a dataframe with plant ids
+        label_df (df): a dataframe with plant ids and other columns that
+            give the location info for the plant
+        labels (list): one or more columns to add to the original dataframe
+    """
+
+    merge_cols = ['plant id'] + labels
+
+    df = df.merge(label_df.loc[:, merge_cols], on='plant id')
+
+    return df
