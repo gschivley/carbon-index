aa3af3b3971c0a0a374f93ebf52b65f372b6dda4
Greg Schivley <greg.schivley@gmail.com>
2019-10-03 09:33:46 -0400

    WIP. Calculate/save national & nerc results.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
 from src.data.cems import download_cems
 from src.data.eia_bulk_extract import extract_all_bulk_data
-from src.analysis.calc_national_nerc_index import CarbonIndex
+from src.data.region_labels import write_region_labels
+
+
+def main():
+
+    download_cems()
+    extract_all_bulk_data()
+    from src.analysis.calc_national_nerc_index import CarbonIndex
+
+    ci = CarbonIndex()
+    ci.calc_national_index()
+    ci.calc_national_gen_intensity()
+    ci.calc_nerc_index()
+    ci.save_files()
+
+
+if __name__ == "__main__":
+    main()
